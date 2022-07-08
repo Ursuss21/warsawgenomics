778b29e7cb290aade839bd8dddd04c55afab25e8
Ursuss21 <robert.skrzypczak21@gmail.com>
2022-07-08 15:01:31 +0200

    Add header, map, search-bar and textarea components. Add flag buttons to header. Handle language buttons click events.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MapComponent } from './map/map.component';
+import { TextareaComponent } from './textarea/textarea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    MapComponent,
+    TextareaComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  selectedLanguage: string = 'pl';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  selectLanguage(lang: string) {
+    this.selectedLanguage = lang;
+  }
+}
